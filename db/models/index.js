4c435eba55eafc5a065a80b13aaad144102b9c75
Kaushik Nagaraj <kaushikstan@gmail.com>
2017-01-07 17:48:35 -0500

    cleanup

--- db/models/index.js
+++ db/models/index.js
@@ -4,16 +4,22 @@
 // so any other part of the application could call sequelize.model('User')
 // to get access to the User model.
 
-const User = require('./user')
-const Product = require('./product')
-const Review = require('./review')
+const User = require('./user');
+const Product = require('./product');
+const Review = require('./review');
+const Order = require('./order');
 
-Product.hasMany(Review)
-User.hasMany(Review)
+Product.hasMany(Review);
+User.hasMany(Review);
 
-Review.belongsTo(Product)
-// User.hasMany(Order)
-// Order.hasMany(Product)
-// Product.belongsTo(Order)
+Review.belongsTo(Product);
+// User.hasMany(Order);
+// Order.hasMany(Product);
+// Product.belongsTo(Order);
 
-module.exports = { User, Product, Review }
+module.exports = {
+  User,
+  Product,
+  Review,
+  Order
+};
