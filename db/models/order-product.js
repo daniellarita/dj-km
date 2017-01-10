2a39ffeda307e3927bae5f8dfff84bd418c9a8e6
Kaushik Nagaraj <kaushikstan@gmail.com>
2017-01-10 18:53:39 -0500

    orders model

--- db/models/order-product.js
+++ db/models/order-product.js
@@ -8,10 +8,12 @@ const OrderProduct = db.define('order_products', {
     type: Sequelize.INTEGER,
     defaultValue: 0
   },
+
   price: {
     type: Sequelize.FLOAT,
     defaultValue: 0
   },
+  
   totalAmount: {
     // this is price * quantity per product
     type: Sequelize.FLOAT,
