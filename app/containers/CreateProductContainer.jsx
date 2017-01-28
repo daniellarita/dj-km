fbb24cb9d17c526dbc8cf6affb0d70865fc4e3b8
Daniella Baxter <dbaxter@nyc-047571-mbp.local>
2017-01-28 12:41:32 -0500

    multi-tenancy

--- app/containers/CreateProductContainer.jsx
+++ app/containers/CreateProductContainer.jsx
@@ -11,9 +11,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    submit: function(event) {
-      event.preventDefault();
-      dispatch(postProduct());
+    postProduct: function() {
+      dispatch(postProduct(newProduct));
     }
 
   };
@@ -22,4 +21,108 @@ const mapDispatchToProps = (dispatch) => {
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(CreateProduct);
+)(
+  class extends React.Component{
+    constructor(){
+      super();
+      this.state={
+        newProduct:{
+          artistName:'',
+          description:'',
+          email:'',
+          price:0,
+          image:'',
+          genre:'',
+          location:'',
+          audio:'',
+          quantity:0
+        }
+      }
+      this.handleSubmit=this.handleSubmit.bind(this);
+      this.handleChange=this.handleChange.bind(this);
+    }
+
+    handleSubmit(e){
+      e.preventDefault();
+    }
+
+    handleChange(e){
+      let temp=this.state.newProduct;
+
+      if (e.target.name==="artistName") {
+        temp.artistName=e.target.value;
+        this.setState({
+          newProduct: temp
+        })
+      }
+
+      if (e.target.name==="description") {
+        temp.description=e.target.value;
+        this.setState({
+          newProduct: temp
+        })
+      }
+
+      if (e.target.name==="email") {
+        temp.email=e.target.value;
+        this.setState({
+          newProduct: temp
+        })
+      }
+
+      if (e.target.name==="price") {
+        temp.price=e.target.value;
+        this.setState({
+          newProduct: temp
+        })
+      }
+
+      if (e.target.name==="image") {
+        temp.image=e.target.value;
+        this.setState({
+          newProduct: temp
+        })
+      }
+
+      if (e.target.name==="genre") {
+        temp.genre=e.target.value;
+        this.setState({
+          newProduct: temp
+        })
+      }
+
+      if (e.target.name==="location") {
+        temp.location=e.target.value;
+        this.setState({
+          newProduct: temp
+        })
+      }
+
+      if (e.target.name==="audio") {
+        temp.audio=e.target.value;
+        this.setState({
+          newProduct: temp
+        })
+      }
+
+      if (e.target.name==="quantity") {
+        temp.quantity=e.target.value;
+        this.setState({
+          newProduct: temp
+        })
+      }
+    }
+
+    render(){
+      return(
+        <CreateProduct
+          {...this.state}
+          {...this.props}
+          submit={this.handleSubmit}
+          handleChange={this.handleChange}
+        />
+      );
+    }
+  }
+)
+
